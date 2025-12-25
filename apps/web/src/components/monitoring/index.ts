9ca3bbac6e14c20e418385600dc34160433498fc
clement893 <clement@nukleo.com>
2025-12-25 10:39:46 -0500

    Fix: remove non-existent Props type exports from monitoring components index

--- apps/web/src/components/monitoring/index.ts
+++ apps/web/src/components/monitoring/index.ts
@@ -11,12 +11,3 @@ export { default as SystemPerformanceDashboard } from './SystemPerformanceDashbo
 export { default as PerformanceProfiler } from './PerformanceProfiler';
 export { default as SystemMetrics } from './SystemMetrics';
 
-export type { AlertsPanelProps } from './AlertsPanel';
-export type { ErrorTrackingDashboardProps } from './ErrorTrackingDashboard';
-export type { HealthStatusProps } from './HealthStatus';
-export type { LogsViewerProps } from './LogsViewer';
-export type { MetricsChartProps } from './MetricsChart';
-export type { SystemPerformanceDashboardProps } from './SystemPerformanceDashboard';
-export type { PerformanceProfilerProps } from './PerformanceProfiler';
-export type { SystemMetricsProps } from './SystemMetrics';
-
