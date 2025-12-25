df2dd664e0115a3c3ce67e0c4b2481a3a7885ee3
clement893 <clement@nukleo.com>
2025-12-25 10:50:14 -0500

    Fix: correct PricingCard and PricingSection exports (use named exports instead of default)

--- apps/web/src/components/subscriptions/index.ts
+++ apps/web/src/components/subscriptions/index.ts
@@ -3,11 +3,9 @@
  * Note: PaymentHistory is exported from @/components/billing
  */
 
-export { default as PricingCard } from './PricingCard';
-export { default as PricingSection } from './PricingSection';
+export { PricingCard } from './PricingCard';
+export { PricingSection } from './PricingSection';
 export { default as SubscriptionCard } from './SubscriptionCard';
 
-export type { PricingCardProps } from './PricingCard';
-export type { PricingSectionProps } from './PricingSection';
-export type { SubscriptionCardProps } from './SubscriptionCard';
+export type { Plan } from './PricingCard';
 
