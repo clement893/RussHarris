9fc57ee2372220186b1fcc52e44da8b1400b07b0
clement893 <clement@nukleo.com>
2025-12-25 09:51:43 -0500

    Fix: remove non-existent LanguageSwitcherProps type export

--- apps/web/src/components/i18n/index.ts
+++ apps/web/src/components/i18n/index.ts
@@ -6,7 +6,6 @@ export { default as LanguageSwitcher } from './LanguageSwitcher';
 export { default as LocaleSwitcher } from './LocaleSwitcher';
 export { default as RTLProvider } from './RTLProvider';
 
-export type { LanguageSwitcherProps } from './LanguageSwitcher';
 export type { LocaleSwitcherProps } from './LocaleSwitcher';
 export type { RTLProviderProps } from './RTLProvider';
 
