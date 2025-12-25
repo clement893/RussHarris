ca331e14ae465d35bca19958aaef315c8a0062a8
clement893 <clement@nukleo.com>
2025-12-25 10:42:26 -0500

    Fix: correct PerformanceScripts and WebVitalsReporter exports and remove non-existent Props types

--- apps/web/src/components/performance/index.ts
+++ apps/web/src/components/performance/index.ts
@@ -5,12 +5,10 @@
 export { default as OfflineSupport } from './OfflineSupport';
 export { default as OptimisticUpdates } from './OptimisticUpdates';
 export { default as OptimizationDashboard } from './OptimizationDashboard';
-export { default as PerformanceScripts } from './PerformanceScripts';
-export { default as WebVitalsReporter } from './WebVitalsReporter';
+export { PerformanceScripts } from './PerformanceScripts';
+export { WebVitalsReporter } from './WebVitalsReporter';
 
 export type { OfflineSupportProps } from './OfflineSupport';
 export type { OptimisticUpdatesProps } from './OptimisticUpdates';
 export type { PerformanceDashboardProps } from './OptimizationDashboard';
-export type { PerformanceScriptsProps } from './PerformanceScripts';
-export type { WebVitalsReporterProps } from './WebVitalsReporter';
 
