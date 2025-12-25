4f47f88f0ad9aa3449d980d91a17ec22eff6161c
clement893 <clement@nukleo.com>
2025-12-25 10:43:50 -0500

    Fix: remove non-existent Props type exports from providers index

--- apps/web/src/components/providers/index.ts
+++ apps/web/src/components/providers/index.ts
@@ -7,8 +7,3 @@ export { default as QueryProvider } from './QueryProvider';
 export { default as SessionProvider } from './SessionProvider';
 export { default as ThemeManagerProvider } from './ThemeManagerProvider';
 
-export type { AppProvidersProps } from './AppProviders';
-export type { QueryProviderProps } from './QueryProvider';
-export type { SessionProviderProps } from './SessionProvider';
-export type { ThemeManagerProviderProps } from './ThemeManagerProvider';
-
